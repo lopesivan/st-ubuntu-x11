5f551e4e4fe54a2407878506f7a7a7a35be0eb26
Ivan Lopes <lopesivan.ufrj@gmail.com>
2026-09-23 18:50:38 -0300

    fix: atualiza hb.h

--- hb.h
+++ hb.h
@@ -1,7 +1,12 @@
+#ifndef ST_HB_H
+#define ST_HB_H
+
 #include <X11/Xft/Xft.h>
 // #include <hb.h>
 #include <hb-ft.h>
 
 void hbunloadfonts();
 void hbtransform(XftGlyphFontSpec *, const Glyph *, size_t, int, int);
+
+#endif		// ST_HB_H
 
